889462758109f441febeb7bb5a573c1f9728ac62
Ryan Purse <ryanpurse@gmail.com>
2021-04-20 09:24:22 +0100

    Created a Barrier Entity

---
+++ include/entities/Barrier.h
@@ -0,0 +1,32 @@
+/**
+ * @file Barrier.h
+ * @brief [fill in this secton]
+ * Project: A2OOPGame
+ * @author Ryan Purse
+ * @version 1.0.0
+ * Initial Version: 19/04/2021
+ */
+
+
+#ifndef A2OOPGAME_BARRIER_H
+#define A2OOPGAME_BARRIER_H
+
+#include "Entity.h"
+#include <glm.hpp>
+
+/**
+ * The Wall for the arena that entities are held in.
+ * @author Ryan Purse
+ * @version 1.0.0 [Initial Version: 19/04/2021]
+ */
+class Barrier : public Entity
+{
+public:
+    explicit Barrier(const glm::vec2 &position=glm::vec2(0.f));
+
+    void update() override;
+    void onCollision(const std::shared_ptr<Entity> &other) override;
+};
+
+
+#endif //A2OOPGAME_BARRIER_H
---
+++ src/entities/Barrier.cpp
@@ -0,0 +1,28 @@
+/**
+ * @file Barrier.cpp.cc
+ * @brief [fill in this secton]
+ * Project: A2OOPGame
+ * @author Ryan Purse
+ * @version 1.0.0
+ * Initial Version: 19/04/2021
+ */
+
+
+#include "Barrier.h"
+#include "QuadTreeHelpers.h"
+
+Barrier::Barrier(const glm::vec2 &position) :
+    Entity(position, glm::vec2(4096), quad::layers::Boundary, "../tmp/TheRing-0001.png")
+{
+    mTransform.scale = glm::vec2(4.f);
+}
+
+void Barrier::update()
+{
+
+}
+
+void Barrier::onCollision(const std::shared_ptr<Entity> &other)
+{
+
+}
