c7fbed8275872ae34af118bb6e9729e48f945ac8
Ryan Purse <ryanpurse@gmail.com>
2021-05-03 09:05:56 +0100

    Added comments.

--- include/entities/Entity.h
+++ include/entities/Entity.h
@@ -56,20 +56,48 @@ public:
     glm::vec2 getHitBoxCenter() const;
 
 protected:
+    /**
+     * Stops an entity from going out of bounds. Upon collision, it will
+     * move the entity back to a valid place.
+     * @param barrierCollider The bounding box that you want to test against.
+     */
     void pushOffWall(const std::shared_ptr<BarrierCollider> &barrierCollider);
 
 public:  // todo: convert this to protected fields.
+    /** The position, rotation and size of the entity */
     transform mTransform;
+
+    /** The velocity of the entity. Defined here so that the renderer can 'predict' where the entity will go */
     glm::vec2 mVelocity;
+
+    /** The image displayed by the renderer. */
     const std::string mImageRef;
+
+    /** Offset from the top-left most corner. */
     glm::vec2 mHitBoxOffset;
+
+    /** How big the hit box is. */
     glm::vec2 mHitBoxSize;
+
+    /** Used for debugging only. */
     glm::vec4 mHitBoxColour;
+
+    /** What collision layer it is attached to. Used quad::layers to set this field. */
     const size_t mCollisionLayer;
+
+    /** Flag for clean up an the end of a frame. */
     bool mIsDead;
+
+    /** Toggle for the hit box */
     bool mIsCollidable;
+
+    /** Toggle for the renderer to display an image */
     bool mIsRenderable;
+
+    /** Rotational velocity */
     double mAngularVelocity;
+
+    /** The layers in which this entity can collide with. */
     size_t mQueryLayers;
 };
 
