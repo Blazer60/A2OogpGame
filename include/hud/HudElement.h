dc74a7810f015665fe9b59736bc7d3029fa1f01a
Ryan Purse <ryanpurse@gmail.com>
2021-05-04 09:15:03 +0100

    Added a HUD Element

---
+++ include/hud/HudElement.h
@@ -0,0 +1,61 @@
+/**
+ * @file HudElement.h
+ * @brief Abstract base class for elements that are
+ * not impacted by the position of the camera.
+ * Project: A2OOPGame
+ * @author Ryan Purse
+ * @version 1.0.0
+ * Initial Version: 04/05/2021
+ */
+
+
+#ifndef A2OOPGAME_HUDELEMENT_H
+#define A2OOPGAME_HUDELEMENT_H
+
+#include <glm.hpp>
+
+/**
+ * Transforms for HUD Elements. Similar to Transform
+ * but uses integers for its position.
+ * @see transform
+ */
+struct hudTransform
+{
+    glm::ivec2 position;
+    double rotation;
+    glm::vec2 scale;
+
+    hudTransform(const glm::ivec2 &position, double rotation, const glm::vec2 &scale);
+};
+
+
+/**
+ * Abstract base class for elements that are
+ * not impacted by the position of the camera.
+ * @see Entity
+ * @author Ryan Purse
+ * @version 1.0.0 [Initial Version: 04/05/2021]
+ */
+class HudElement
+{
+    enum anchorPoints : char {
+        Top     = 0b0000,
+        Bottom  = 0b0001,
+        Center  = 0b0010,
+        Left    = 0b0000,
+        Right   = 0b0100,
+        Middle  = 0b1000
+    };
+public:
+    explicit HudElement(const glm::ivec2 &position, char anchorPoint=0);
+    virtual ~HudElement() = default;
+
+    virtual void update() = 0;
+
+protected:
+    hudTransform mTransform;
+    char mAnchorPoint;
+};
+
+
+#endif //A2OOPGAME_HUDELEMENT_H
---
+++ src/hud/HudElement.cpp
@@ -0,0 +1,24 @@
+/**
+ * @file HudElement.cpp.cc
+ * @brief Abstract base class for elements that are
+ * not impacted by the position of the camera.
+ * Project: A2OOPGame
+ * @author Ryan Purse
+ * @version 1.0.0
+ * Initial Version: 04/05/2021
+ */
+
+
+#include "HudElement.h"
+
+hudTransform::hudTransform(const glm::ivec2 &position, double rotation, const glm::vec2 &scale) : position(position),
+                                                                                                  rotation(rotation),
+                                                                                                  scale(scale)
+{}
+
+HudElement::HudElement(const glm::ivec2 &position, char anchorPoint) :
+        mTransform(position, 0, glm::vec2(1)),
+        mAnchorPoint(anchorPoint)
+{
+
+}
