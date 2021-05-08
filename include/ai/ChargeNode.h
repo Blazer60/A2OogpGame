e5bf41f1ff8ee745f4a9bc2f945950ae1c3b8281
Ryan Purse <ryanpurse@gmail.com>
2021-05-08 15:12:13 +0100

    Created Charge Node

---
+++ include/ai/ChargeNode.h
@@ -0,0 +1,27 @@
+/**
+ * @file ChargeNode.h
+ * @brief A node that makes mecha chad charge towards its target.
+ * Project: A2OOPGame
+ * @author Ryan Purse
+ * @version 1.0.0
+ * Initial Version: 08/05/2021
+ */
+
+
+#ifndef A2OOPGAME_CHARGENODE_H
+#define A2OOPGAME_CHARGENODE_H
+
+#include "Node.h"
+
+/**
+ * A node that makes mecha chad charge towards its target.
+ * @author Ryan Purse
+ * @version 1.0.0 [Initial Version: 08/05/2021]
+ */
+class ChargeNode : public Node
+{
+
+};
+
+
+#endif //A2OOPGAME_CHARGENODE_H
---
+++ src/ai/ChargeNode.cpp
@@ -0,0 +1,11 @@
+/**
+ * @file ChargeNode.cpp.cc
+ * @brief A node that makes mecha chad charge towards its target.
+ * Project: A2OOPGame
+ * @author Ryan Purse
+ * @version 1.0.0
+ * Initial Version: 08/05/2021
+ */
+
+
+#include "ChargeNode.h"
